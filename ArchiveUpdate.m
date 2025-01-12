b9cbd1f75b51ef449ca2f07942b583fbc64a9b91
张勇 <116160306+zooooooy@users.noreply.github.com>
2025-01-12 11:26:13 +0800

    Update ArchiveUpdate.m

--- ArchiveUpdate.m
+++ ArchiveUpdate.m
@@ -1,14 +1,6 @@
 function Population = ArchiveUpdate(Population,N)
 % Update archive
 
-%------------------------------- Copyright --------------------------------
-% Copyright (c) 2021 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
-% for evolutionary multi-objective optimization [educational forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
-%--------------------------------------------------------------------------
     Population1=Population;
     %% Select feasible solutions
     fIndex     = all(Population.cons <= 0,2);
