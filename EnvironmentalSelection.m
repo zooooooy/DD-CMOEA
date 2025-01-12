0130314edf645addd572a2319590a6e1e661c204
张勇 <116160306+zooooooy@users.noreply.github.com>
2025-01-12 11:26:41 +0800

    Update EnvironmentalSelection.m

--- EnvironmentalSelection.m
+++ EnvironmentalSelection.m
@@ -1,17 +1,7 @@
 function [Population,Fitness] = EnvironmentalSelection(Population,N,isOrigin)
-% The environmental selection of SPEA2
 %Population：当前种群的对象集合。
 %N：选择的解决方案数量。
 %isOrigin：一个布尔值，指示是否考虑约束条件。
-
-%------------------------------- Copyright --------------------------------
-% Copyright (c) 2023 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
-% for evolutionary multi-objective optimization [educational forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
-%--------------------------------------------------------------------------
 
     %% Calculate the fitness of each solution
     if isOrigin 
