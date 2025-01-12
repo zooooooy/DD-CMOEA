013ab961e8e6938f5312520ced6908411ece88b4
张勇 <116160306+zooooooy@users.noreply.github.com>
2025-01-12 11:27:20 +0800

    Update EnvironmentalSelection_VAR.m

--- EnvironmentalSelection_LAT.m
+++ EnvironmentalSelection_LAT.m
@@ -1,16 +1,4 @@
-function [return_pop,return_Fitness] = EnvironmentalSelection_LAT(Population,N,VAR)
-% Multi-objective-based CHT is used to sort the Population of local auxiliary task
-
-%------------------------------- Copyright --------------------------------
-% Copyright (c) 2023 BIMK Group. You are free to use the PlatEMO for
-% research purposes. All publications which use this platform or any code
-% in the platform should acknowledge the use of "PlatEMO" and reference "Ye
-% Tian, Ran Cheng, Xingyi Zhang, and Yaochu Jin, PlatEMO: A MATLAB platform
-% for evolutionary multi-objective optimization [educational forum], IEEE
-% Computational Intelligence Magazine, 2017, 12(4): 73-87".
-%--------------------------------------------------------------------------
-
-% This function is written by Kangjia Qiao (email: qiaokangjia@yeah.net)
+function [return_pop,return_Fitness] = EnvironmentalSelection_VAR(Population,N,VAR)
 
     input_cons = Population.cons;
     input_cons(input_cons<0) = 0;
